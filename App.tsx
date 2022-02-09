a267b3026958c9660405cf923ee200d03f8801d0
mariaclarabarbosa <clara_estrella@protonmail.com>
2022-02-09 08:34:48 -0300

    Implementação do styled components

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import theme from './src/global/styles/theme';
 import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
-  return <Dashboard />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  )
 }
