680ea94fdca9534c7ff500005e2f484fcf554a45
Maria Clara Barbosa <maria.estrella@neki-it.com.br>
2022-03-08 17:23:37 -0300

    Implementação da tab navigation

--- App.tsx
+++ App.tsx
@@ -10,8 +10,10 @@ import {
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
-import { Dashboard } from './src/screens/Dashboard';
-import { Register } from './src/screens/Register';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { AppRoutes } from './src/routes/app.routes';
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,8 +28,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {/* <Dashboard /> */}
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   )
 }
